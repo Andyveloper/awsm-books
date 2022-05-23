b3b4eefc41e2c0636f6c2bdb7c9b9d6a64bf558c
Gabriel <gopxfs@hotmail.com>
2022-05-23 16:16:21 -0300

    Update Add book function

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,27 +3,31 @@ const form = document.querySelector('.add-book__add-form')
 const title = document.querySelector('#book-title');
 const author = document.querySelector('#book-author');
 const submitBtn = document.querySelector('#book-submit');
-
 const bookLibrary = [];
 
-function saveBook() {
-  const book = new Object() 
+submitBtn.addEventListener('click', (event) => {
+  let book = new Object();
   book.title = title.value;
   book.author = author.value;
-  return bookLibrary.push(book);
-}
+  bookLibrary.push(book);
+  const listBook = document.createElement('li');
+  const bookDiv = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const removeButton = document.createElement('button');
+  removeButton.innerHTML = 'Remove';
+  removeButton.classList.add('remove-button');
+  listBook.appendChild(bookDiv);
+  bookDiv.append(bookTitle, bookAuthor, removeButton);
+  bookContainer.appendChild(listBook);
+  bookTitle.innerHTML = book.title;
+  bookAuthor.innerHTML = book.author;
+});
 
-// form.submit(saveBook());
+// removeButton.addEventListener('click', (event) => ) {
+
+// } 
 
 // when click on button
 // save title and author into object
 // object must be in array
-
-let whatever = {
-  nombre: 'camilo',
-  apellido: 'Rodriguez'
-}
-
-let arreglo = [];
-
-arreglo.push(whatever);
