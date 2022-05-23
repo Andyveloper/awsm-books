0f4a85e77b19bacd13063b8271eaeed65598a05b
Gabriel <gopxfs@hotmail.com>
2022-05-23 18:40:06 -0300

    Finish remove button

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,14 +3,13 @@ const form = document.querySelector('.add-book__add-form')
 const title = document.querySelector('#book-title');
 const author = document.querySelector('#book-author');
 const submitBtn = document.querySelector('#book-submit');
-const bookLibrary = [];
-const removeButtons = [];
+const bookLibrary = []; // store LI instead of book title / author
+let removeButtons = [];
 
 submitBtn.addEventListener('click', (event) => {
   let book = new Object();
   book.title = title.value;
   book.author = author.value;
-  bookLibrary.push(book);
   const listBook = document.createElement('li');
   const bookDiv = document.createElement('div');
   const bookTitle = document.createElement('p');
@@ -18,24 +17,21 @@ submitBtn.addEventListener('click', (event) => {
   const removeButton = document.createElement('button');
   removeButton.innerHTML = 'Remove';
   removeButton.classList.add('remove-button');
-  // removeBook(removeButton);
+  removeBook(removeButton);
   removeButtons.push(removeButton);
   listBook.appendChild(bookDiv);
   bookDiv.append(bookTitle, bookAuthor, removeButton);
   bookContainer.appendChild(listBook);
   bookTitle.innerHTML = book.title;
   bookAuthor.innerHTML = book.author;
+  bookLibrary.push(listBook);
 });
 
-
-const removeBook = () => {
-  const deleteButton = document.querySelectorAll['.remove-button'];
-  for (let i = 0; i < bookLibrary.length; i++) {
-    deleteButton[i].addEventListener('click', () => {
-      if (deleteButton[i] === bookLibrary[i]) {
-        console.log('a');
-      }
-
-    })
-  }
+function removeBook(button) {
+  button.addEventListener('click', (event) => {
+    // remove the index of the LI
+    bookContainer.removeChild(bookLibrary[removeButtons.indexOf(button)]);
+    bookLibrary.splice(removeButtons.indexOf(button), 1);
+    removeButtons.splice(removeButtons.indexOf(button), 1);
+  });
 }
