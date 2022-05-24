52dcdb044ba778535892a6b36037dbb799714f77
Gabriel <gopxfs@hotmail.com>
2022-05-23 21:11:29 -0300

    Update whole code

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,37 +1,48 @@
+// getting html elements
 const bookContainer = document.querySelector('.book-list__container');
 const form = document.querySelector('.add-book__add-form')
 const title = document.querySelector('#book-title');
 const author = document.querySelector('#book-author');
 const submitBtn = document.querySelector('#book-submit');
-const bookLibrary = []; // store LI instead of book title / author
-let removeButtons = [];
+let bookLibrary = [];
 
+// submit button function
 submitBtn.addEventListener('click', (event) => {
-  let book = new Object();
-  book.title = title.value;
-  book.author = author.value;
+  createNewBookDiv(title.value, author.value);
+});
+
+function createNewBookDiv(title, author) {
+  // creating html elements
   const listBook = document.createElement('li');
   const bookDiv = document.createElement('div');
   const bookTitle = document.createElement('p');
   const bookAuthor = document.createElement('p');
   const removeButton = document.createElement('button');
+  const newBook = new Object;
+  // creating new book
+  newBook.title = title;
+  newBook.author = author;
+  newBook.index = Date.now();
+  listBook.setAttribute('id', `book${newBook.index}`);
+  bookLibrary.push(newBook);
+  // settings html elements
   removeButton.innerHTML = 'Remove';
   removeButton.classList.add('remove-button');
-  removeBook(removeButton);
-  removeButtons.push(removeButton);
+  removeButton.setAttribute('id', `button${newBook.index}`);
+  removeButton.addEventListener('click', removeBookDiv(removeButton.id));
+  bookContainer.appendChild(listBook);
   listBook.appendChild(bookDiv);
   bookDiv.append(bookTitle, bookAuthor, removeButton);
-  bookContainer.appendChild(listBook);
-  bookTitle.innerHTML = book.title;
-  bookAuthor.innerHTML = book.author;
-  bookLibrary.push(listBook);
-});
+  bookTitle.innerHTML = newBook.title;
+  bookAuthor.innerHTML = newBook.author;
+}
 
-function removeBook(button) {
-  button.addEventListener('click', (event) => {
-    // remove the index of the LI
-    bookContainer.removeChild(bookLibrary[removeButtons.indexOf(button)]);
-    bookLibrary.splice(removeButtons.indexOf(button), 1);
-    removeButtons.splice(removeButtons.indexOf(button), 1);
-  });
+function removeBookDiv(index) {
+  const div = document.getElementById(`button${index}`);
+  bookContainer.removeChild(div);
+  for (let i = 0; i < bookLibrary.length; i++) {
+    if (bookLibrary[i].index === `book${index}`) {
+      bookLibrary.splice(i,1);
+    }
+  }
 }
