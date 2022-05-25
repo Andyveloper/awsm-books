b1a3be798b9c8296977766dcc8c468134ee956fc
Gabriel <gopxfs@hotmail.com>
2022-05-25 15:39:10 -0300

    Hide correct part of the page when clicking the navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -99,32 +99,31 @@ const populateData = () => {
 };
 populateData();
 
+// Creating html elements to hide sessions
 const navBarAnchors = document.getElementsByClassName('navbar-list__items');
 const bookSection = document.getElementsByClassName('book-list');
 const addBookSection = document.getElementsByClassName('add-book');
 const contactSection = document.getElementsByClassName('contact');
+// Show the content that is clicked
 function showContent(position) {
   if (position == 0) {
-    if (bookSection[0].classList.contains('hideAll')) {
       bookSection[0].classList.remove('hideAll');
       addBookSection[0].classList.add('hideAll');
       contactSection[0].classList.add('hideAll');
-    }
   } else if (position == 1) {
-    if (bookSection[0].classList.contains('hideAll')) {
       bookSection[0].classList.add('hideAll');
       addBookSection[0].classList.remove('hideAll');
       contactSection[0].classList.add('hideAll');
-    }
   } else if (position == 2) {
-    if (bookSection[0].classList.contains('hideAll')) {
       bookSection[0].classList.add('hideAll');
       addBookSection[0].classList.add('hideAll');
       contactSection[0].classList.remove('hideAll');
-    }
   }
 }
+// Giving the event listener to the anchor tags
 for (let i = 0; i < navBarAnchors.length; i += 1 ) {
-  navBarAnchors[i].addEventListener('click', showContent(i));
+  navBarAnchors[i].addEventListener('click', () => {
+    showContent(i);
+  });
 }
 
