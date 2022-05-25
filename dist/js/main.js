cc292a28de3bd929e5f17cf2a19bc6f62c18f297
Gabriel <gopxfs@hotmail.com>
2022-05-25 15:56:56 -0300

    Update linter errors

--- dist/js/main.js
+++ dist/js/main.js
@@ -106,24 +106,23 @@ const addBookSection = document.getElementsByClassName('add-book');
 const contactSection = document.getElementsByClassName('contact');
 // Show the content that is clicked
 function showContent(position) {
-  if (position == 0) {
-      bookSection[0].classList.remove('hideAll');
-      addBookSection[0].classList.add('hideAll');
-      contactSection[0].classList.add('hideAll');
-  } else if (position == 1) {
-      bookSection[0].classList.add('hideAll');
-      addBookSection[0].classList.remove('hideAll');
-      contactSection[0].classList.add('hideAll');
-  } else if (position == 2) {
-      bookSection[0].classList.add('hideAll');
-      addBookSection[0].classList.add('hideAll');
-      contactSection[0].classList.remove('hideAll');
+  if (position === 0) {
+    bookSection[0].classList.remove('hideAll');
+    addBookSection[0].classList.add('hideAll');
+    contactSection[0].classList.add('hideAll');
+  } else if (position === 1) {
+    bookSection[0].classList.add('hideAll');
+    addBookSection[0].classList.remove('hideAll');
+    contactSection[0].classList.add('hideAll');
+  } else if (position === 2) {
+    bookSection[0].classList.add('hideAll');
+    addBookSection[0].classList.add('hideAll');
+    contactSection[0].classList.remove('hideAll');
   }
 }
 // Giving the event listener to the anchor tags
-for (let i = 0; i < navBarAnchors.length; i += 1 ) {
+for (let i = 0; i < navBarAnchors.length; i += 1) {
   navBarAnchors[i].addEventListener('click', () => {
     showContent(i);
   });
 }
-
